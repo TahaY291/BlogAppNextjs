b6af609dee05926e5d7263742e8a1f44ad6f03bf
TahaY291 <mtahayasin07@gmail.com>
2025-11-06 14:13:44 +0500

    wrong import resolved

--- app/api/auth/update-user/route.js
+++ app/api/auth/update-user/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
 import connectCloudinary from "@/lib/cloudinary";
 import { v2 as cloudinary } from "cloudinary";
